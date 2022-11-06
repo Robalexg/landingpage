5633a0a17c7f00db2aabe234a1d4b9500fec8cc4
robalexg <alexg7070@yahoo.com>
2022-11-05 23:29:05 -0500

    courses

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -3,15 +3,15 @@ import './Course.css'
 const Course = () => {
     return (
         <section className='course--container'> 
-            <img src="../images/icon-animation.svg" alt="" />
-            <h1>
+            <img className='course--image' src="../images/icon-animation.svg" alt="" />
+            <h1 className='course--name'>
                 Animation
             </h1>
-            <h2>
+            <h2 className='course--desc'>
                 Learn the latest animation techniques to create 
                 stunning motion design and captivate your audience.
             </h2>
-            <h3>
+            <h3 className='course--start'>
                 Get Started
             </h3>
         </section>
